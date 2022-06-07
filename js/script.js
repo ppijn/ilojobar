e3189811b7012fd23ca6d9b4b3e6a53c4105c29e
juliandecloe <julian.de.cloe@hva.nl>
2022-06-07 11:43:24 +0200

    remove all errors

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
-if(window.location.href.includes('bar.html')) {
+if (window.location.href.includes('bar.html')) {
     var scene = document.getElementById("scene");
     var parallaxInstance = new Parallax(scene);
 }
 
-const input = document.querySelector('.switch input');
-const canvas = document.querySelector('.three-module');
-const img = document.querySelector('.ilojobar');
-let checker;
+if (window.location.href.includes('index.html')) {
+    const input = document.querySelector('.switch input');
+    const canvas = document.querySelector('.three-module');
+    const img = document.querySelector('.ilojobar');
+    let checker;
 
-function toggleThree() {
-    if (!checker) {
-        console.log('block')
-        img.style.setProperty('display', 'none');
-        canvas.style.setProperty('display', 'block');
-        checker = true
-    } else {
-        console.log('none')
-        canvas.style.setProperty('display', 'none');
-        img.style.setProperty('display', 'block');
-        checker = false;
+    function toggleThree() {
+        if (!checker) {
+            console.log('block')
+            img.style.setProperty('display', 'none');
+            canvas.style.setProperty('display', 'block');
+            checker = true
+        } else {
+            console.log('none')
+            canvas.style.setProperty('display', 'none');
+            img.style.setProperty('display', 'block');
+            checker = false;
+        }
     }
-}
 
-input.addEventListener('change', toggleThree);
+    input.addEventListener('change', toggleThree);
+}
